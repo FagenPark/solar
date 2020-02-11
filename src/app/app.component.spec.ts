763b76290c54756aa2d46dc113f7add1f06cf492
jsun <jsun@choice.com.au>
2020-02-11 16:11:58 +1100

    unit testing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('solar');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('solar app is running!');
-  });
 });
