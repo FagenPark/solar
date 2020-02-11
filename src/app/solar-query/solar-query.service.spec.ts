201b8e0c4c25330e84bc76938001c6706a10c80a
jsun <jsun@choice.com.au>
2020-02-11 16:12:55 +1100

    unit testing

--- src/app/solar-query/solar-query.service.spec.ts
+++ src/app/solar-query/solar-query.service.spec.ts
@@ -1,9 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
 import { SolarQueryService } from './solar-query.service';
+import {HttpClientModule} from '@angular/common/http';
 
 describe('SolarQueryService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientModule]
+  }));
 
   it('should be created', () => {
     const service: SolarQueryService = TestBed.get(SolarQueryService);
