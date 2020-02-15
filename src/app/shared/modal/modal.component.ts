76e5a4642af8738be9e3fb0027f4c01686c6e21c
jsun <jsun@choice.com.au>
2020-02-15 16:57:30 +1100

    add load spinner

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,23 +1,32 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {Store} from '@ngrx/store';
+import {select, Store} from '@ngrx/store';
 
 import * as fromRoot from 'src/app/state/app.state';
 import * as queryActions from 'src/app/solar-query/state/solar-query.actions';
+import {Observable} from 'rxjs';
+import * as fromSolarQuery from '../../solar-query/state';
+import {trigger} from '@angular/animations';
+import {inOutAnimation} from '../animation,config';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.scss']
+  styleUrls: ['./modal.component.scss'],
+  animations: [
+    trigger(inOutAnimation.name, inOutAnimation.definitions)
+  ],
 })
 export class ModalComponent implements OnInit {
+
+  isModalOpen$: Observable<boolean>;
 
   constructor(private store: Store<fromRoot.State>) { }
 
   ngOnInit() {
+    this.isModalOpen$ = this.store.pipe(select(fromSolarQuery.getModalStatus));
   }
 
   removeModal() {
-    console.log('dada');
     this.store.dispatch(new queryActions.ToggleModal(false));
   }
 
--- src/app/solar-query/solar-query.component.ts
+++ src/app/solar-query/solar-query.component.ts
@@ -5,14 +5,13 @@ import {select, Store} from '@ngrx/store';
 import * as fromRoot from 'src/app/state/app.state';
 import * as fromSolarQuery from './state/';
 import {trigger} from '@angular/animations';
-import {expandCollapseAnimation, inOutAnimation} from '../shared/animation,config';
+import {expandCollapseAnimation} from '../shared/animation,config';
 
 @Component({
   selector: 'app-solar-query',
   templateUrl: './solar-query.component.html',
   styleUrls: ['./solar-query.component.scss'],
   animations: [
-    trigger(inOutAnimation.name, inOutAnimation.definitions),
     trigger(expandCollapseAnimation.name, expandCollapseAnimation.definitions)
   ]
 })
@@ -22,7 +21,6 @@ export class SolarQueryComponent implements OnInit {
   postCode$: Observable<number>;
   numberOfPeople$: Observable<string>;
   modalContentId$: Observable<string>;
-  isModalOpen$: Observable<boolean>;
   stateName$: Observable<string>;
   errorMessage$: Observable<string>;
 
@@ -35,7 +33,6 @@ export class SolarQueryComponent implements OnInit {
     this.postCode$ = this.store.pipe(select(fromSolarQuery.getPostCode));
     this.numberOfPeople$ = this.store.pipe(select(fromSolarQuery.getNumberOfPeople));
     this.modalContentId$ = this.store.pipe(select(fromSolarQuery.getModalContentId));
-    this.isModalOpen$ = this.store.pipe(select(fromSolarQuery.getModalStatus));
     this.stateName$ = this.store.pipe(select(fromSolarQuery.getStateName));
     setTimeout(() => {
       this.isLoading = false;
