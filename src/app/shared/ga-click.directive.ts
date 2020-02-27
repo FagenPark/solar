7d99999457b9a36a09660d10d687bab08e770b39
jsun <jsun@choice.com.au>
2020-02-27 13:58:24 +1100

    add ga events

--- src/app/shared/ga-click.directive.ts
+++ src/app/shared/ga-click.directive.ts
@@ -1,18 +1,18 @@
-import {Directive, ElementRef, HostBinding, HostListener, Input, OnChanges, SimpleChanges} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 
 @Directive({
   selector: '[appGaClick]'
 })
-export class GaClickDirective implements OnChanges {
-
+export class GaClickDirective implements OnInit, OnChanges {
   @HostBinding('attr.data-tracking-category') ctg: string;
   @HostBinding('attr.data-tracking-action') act: string;
   @HostBinding('attr.data-tracking-label') lbl: string;
   @Input('appGaClick') isTrackingClick: boolean;
+  @Input('title') elTitle: string;
   constructor(private el: ElementRef) { }
   @HostListener('click') onClick() {
     if (this.isTrackingClick) {
-      console.log('hostBinding--', this.ctg, this.act, this.el.nativeElement.title);
+      console.log('hostBinding--', this.ctg, this.act, this.lbl);
       // @ts-ignore
       if (window.dataLayer !== undefined) {
         console.log('got datalayer');
@@ -28,8 +28,12 @@ export class GaClickDirective implements OnChanges {
       }
     }
   }
-  ngOnChanges(changes: SimpleChanges): void {
+  ngOnInit(): void {
     this.ctg = 'Button';
     this.act = 'click';
+    this.lbl = this.elTitle;
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    this.lbl = this.elTitle;
   }
 }
