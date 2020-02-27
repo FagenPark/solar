d2c90d9da5c3300c43097a4cb06e45f4218f400b
jsun <jsun@choice.com.au>
2020-02-27 15:13:43 +1100

    add ga events

--- src/app/shared/ga-click.directive.ts
+++ src/app/shared/ga-click.directive.ts
@@ -1,4 +1,4 @@
-import {Directive, ElementRef, HostBinding, HostListener, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {Directive, HostBinding, HostListener, Input, OnInit} from '@angular/core';
 
 @Directive({
   selector: '[appGaClick]'
@@ -12,13 +12,10 @@ export class GaClickDirective implements OnInit {
   constructor() { }
   @HostListener('click') onClick() {
     if (this.isTrackingClick) {
-      console.log('hostBinding--', this.ctg, this.act, this.lbl);
       // @ts-ignore
       if (window.dataLayer !== undefined) {
-        // console.log('got datalayer');
         // @ts-ignore
         window.dataLayer.push({
-          event: 'choiceTrackingEvent',
           choiceTrackingEventDetails: {
             category: this.ctg,
             action: this.act,
@@ -29,6 +26,10 @@ export class GaClickDirective implements OnInit {
     }
   }
   ngOnInit(): void {
+    this.setGAAttrs();
+  }
+
+  private setGAAttrs() {
     this.ctg = 'Button';
     this.act = 'click';
     this.lbl = this.title;
