07093f49becf05c9d8cf8d97cd498739a58edb95
jsun <jsun@choice.com.au>
2020-07-14 13:47:43 +1000

    SM-84

--- src/app/solar-query/postcode-data.ts
+++ src/app/solar-query/postcode-data.ts
@@ -339,36 +339,6 @@ const postCodeData: Postcode[] = [
 export class PostcodeData implements InMemoryDbService {
   createDb() {
     const postcodes = postCodeData;
-    // const postcodes: Postcode[] = [
-    //   {
-    //     id: 2,
-    //     state: 'NSW'
-    //   },
-    //   {
-    //     id: 8,
-    //     state: 'NT'
-    //   },
-    //   {
-    //     id: 3,
-    //     state: 'VIC'
-    //   },
-    //   {
-    //     id: 4,
-    //     state: 'QLD'
-    //   },
-    //   {
-    //     id: 7,
-    //     state: 'TAS'
-    //   },
-    //   {
-    //     id: 6,
-    //     state: 'WA'
-    //   },
-    //   {
-    //     id: 5,
-    //     state: 'SA'
-    //   }
-    // ];
     return {postcodes};
   }
 }
